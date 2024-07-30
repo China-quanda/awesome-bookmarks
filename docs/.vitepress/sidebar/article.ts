7857f5b80fbf6fc00092da92bbc33f7dba761248
China-quanda <864910436@qq.com>
2024-07-30 14:29:32 +0800

    add 电子签名signature

--- docs/.vitepress/sidebar/article.ts
+++ docs/.vitepress/sidebar/article.ts
@@ -38,6 +38,7 @@ export const articleSideBar = [{
       items: [
         { text: '列表', link: '/article/javaScript' },
         { text: 'JavaScript node通过url解析网址 获取 title标题 keywords关键词 description描述 favicon图标', link: '/article/javaScript/JavaScript node通过url解析网址 获取 title标题 keywords关键词 description描述 favicon图标/index' },
+        { text: '电子签名signature', link: '/article/html/电子签名signature/index' },
       ]
     },
     {
@@ -54,6 +55,7 @@ export const articleSideBar = [{
       collapsed: true,
       items: [
         { text: '列表', link: '/article/html' },
+        { text: '电子签名signature', link: '/article/html/电子签名signature/index' },
       ]
     },
     {
